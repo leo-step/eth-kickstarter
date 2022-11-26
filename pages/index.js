0f99df848f87c870e4f9b901922edccc4c0e4aed
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2022-11-26 15:31:09 -0500

    semantic ui progress

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
 import React, { Component } from "react";
+import { Card, Button } from "semantic-ui-react";
 import factory from "../ethereum/factory";
 
 class CampaignIndex extends Component {
-  async componentDidMount() {
+  static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
-    console.log(campaigns);
+    return { campaigns };
+  }
+
+  renderCampaigns() {
+    const items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true,
+      };
+    });
+
+    return <Card.Group items={items} />;
   }
 
   render() {
-    return <div>Campaigns Index</div>;
+    return <div>
+      <h3>Open Campaigns</h3>
+      {this.renderCampaigns()}
+      <Button 
+        content="Create Campaign"
+        icon="add circle"
+        primary
+      />
+    </div>;
   }
 }
 
