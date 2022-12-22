542f34de868d5b38a2360d83ac43b9cdef71b738
Leo Stepanewk <44349262+leo-step@users.noreply.github.com>
2022-12-22 18:45:50 -0500

    create request

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+import { Link } from "../../../routes";
+import Layout from "../../../components/Layout";
 
 class RequestIndex extends Component {
+  static async getInitialProps(props) {
+    const { address } = props.query;
+
+    return { address };
+  }
+
   render() {
-    return <h3>Request List</h3>;
+    return (
+      <Layout>
+        <Link route={`/campaigns/${this.props.address}`}>
+          <a>Back</a>
+        </Link>
+        <h3>Requests</h3>
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <a>
+            <Button primary>Create Request</Button>
+          </a>
+        </Link>
+      </Layout>
+    );
   }
 }
 
